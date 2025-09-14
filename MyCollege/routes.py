0ea6d9a74af83b176cb3efafcbb723ddcb26b842
WhaleRed <redwhalecs@gmail.com>
2025-09-14 21:50:25 +0800

    feat(route): Route to college, programs, students

--- MyCollege/routes.py
+++ MyCollege/routes.py
@@ -3,5 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-  return render_template("base.html")
+@app.route('/colleges')
+def colleges():
+  return render_template("college.html")
+
+@app.route('/programs')
+def programs():
+  return render_template("program.html")
+
+@app.route('/students')
+def students():
+  return render_template("student.html")
