b5238d22fb95d0950ea2b1b0242b09ec6f407bd5
WhaleRed <redwhalecs@gmail.com>
2025-09-15 20:25:17 +0800

    fix(ui): Removed redundant js code for .active in nav-link

--- MyCollege/static/js/script.js
+++ MyCollege/static/js/script.js
@@ -1,13 +1,2 @@
-const navLinks = document.querySelectorAll('.nav-link');
-const sortDropdown = document.querySelectorAll('.dropdown-item');
 
-  navLinks.forEach(link => {
-    link.addEventListener('click', function() {
-      // Remove .active from all links
-      navLinks.forEach(l => l.classList.remove('active'));
-      // Add .active to the clicked one
-      this.classList.add('active');
-    });
-  });
-
-  new DataTable('#example');
+new DataTable('#example');
