b289875180bc9a4717e4d796aaedbb9ff8b4770d
WhaleRed <redwhalecs@gmail.com>
2025-09-15 20:01:57 +0800

    style(ui): Remove js for dropdown

--- MyCollege/static/js/script.js
+++ MyCollege/static/js/script.js
@@ -10,13 +10,4 @@ const sortDropdown = document.querySelectorAll('.dropdown-item');
     });
   });
 
-    sortDropdown.forEach(link => {
-    link.addEventListener('click', function() {
-      // Remove .active from all links
-      sortDropdown.forEach(l => l.classList.remove('active'));
-      // Add .active to the clicked one
-      this.classList.add('active');
-    });
-  });
-
   new DataTable('#example');
