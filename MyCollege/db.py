849a8cc1a44a74d72f6e64d173d32c7bd94bdb88
WhaleRed <redwhalecs@gmail.com>
2025-09-27 22:12:27 +0800

    chore(refactor): Moved db

---
+++ MyCollege/db.py
@@ -0,0 +1,19 @@
+import psycopg2
+from flask import g
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+def get_db():
+    if 'db' not in g:
+        g.db = psycopg2.connect(
+            dbname=DB_NAME,
+            user= DB_USER,
+            password=DB_PASS,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
