7b3391ab2dd3327cbecba5c764022518efa5a803
WhaleRed <redwhalecs@gmail.com>
2025-09-30 16:49:35 +0800

    chore(refactor): Relative import

--- MyCollege/__init__.py
+++ MyCollege/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 def create_app():
   app = Flask(__name__, template_folder="views/templates", static_folder="views/static")
 
-  from .controllers import general_bp as general_blueprint
+  from .controllers.general import general_bp as general_blueprint
   app.register_blueprint(general_blueprint)
   return app
