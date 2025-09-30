a2d5b657bf370a7804973470eb9a71639c579fd8
WhaleRed <redwhalecs@gmail.com>
2025-09-30 19:12:16 +0800

    feat(route): Added college blueprint

--- MyCollege/__init__.py
+++ MyCollege/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from config import SECRET_KEY
 
 def create_app():
   app = Flask(__name__, template_folder="views/templates", static_folder="views/static")
+  app.secret_key = SECRET_KEY
 
   from .controllers.general import general_bp as general_blueprint
+  from .controllers.college import college_bp as college_blueprint
   app.register_blueprint(general_blueprint)
+  app.register_blueprint(college_blueprint)
   return app
