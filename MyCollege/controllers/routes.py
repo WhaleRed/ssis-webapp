76e0cd9b90bea711db9f4c5cbc2b3fc7532c4b85
WhaleRed <redwhalecs@gmail.com>
2025-09-30 15:49:23 +0800

    feat(db): Load data to table

--- MyCollege/controllers/routes.py
+++ MyCollege/controllers/routes.py
@@ -1,16 +1,21 @@
 from flask import render_template
 from . import general_bp
-
+from MyCollege.models.college import *
+from MyCollege.models.program import *
+from MyCollege.models.student import *
 
 @general_bp.route('/', endpoint="colleges")
 @general_bp.route('/colleges')
 def colleges():
-  return render_template("college.html")
+  data = populateCollege(1)
+  return render_template("college.html", data = data)
 
 @general_bp.route('/programs', endpoint="programs")
 def programs():
-  return render_template("program.html")
+  data = populateProgram(1)
+  return render_template("program.html", data = data)
 
 @general_bp.route('/students', endpoint="students")
 def students():
-  return render_template("student.html")
+  data = populateStudent(1)
+  return render_template("student.html", data = data)
