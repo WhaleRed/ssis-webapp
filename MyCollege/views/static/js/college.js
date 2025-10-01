a3bab8fd47bb8764de8b9bbfc218bb8e0db1754f
WhaleRed <redwhalecs@gmail.com>
2025-10-01 18:04:51 +0800

    feat(delete): Delete script for college

--- MyCollege/views/static/js/college.js
+++ MyCollege/views/static/js/college.js
@@ -1,4 +1,5 @@
 const editModal = document.getElementById('editModal');
+const deleteModal = document.getElementById('deleteModal');
 
 if (editModal) {
   editModal.addEventListener('show.bs.modal', function (event) {
@@ -12,5 +13,17 @@ if (editModal) {
     document.getElementById('colCodeInitial').value = code;
     document.getElementById('colCodeEdit').value = code;
     document.getElementById('colNameEdit').value = name;
+  });
+}
+
+if (deleteModal) {
+  deleteModal.addEventListener('show.bs.modal', function (event){
+    const button = event.relatedTarget;
+
+    //Get data from button
+    const code = button.getAttribute('del-code');
+
+    //Fill hidden input
+    document.getElementById('colCodeDelete').value = code;
   });
 }
