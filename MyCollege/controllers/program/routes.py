171c674a8d5467b2cbcc9e6471f3add8532d9556
WhaleRed <redwhalecs@gmail.com>
2025-10-01 16:53:25 +0800

    feat(edit): Edit for program

--- MyCollege/controllers/program/routes.py
+++ MyCollege/controllers/program/routes.py
@@ -14,3 +14,17 @@ def add_program():
     flash('Program Added Succesfully')
 
     return redirect(url_for('general.programs'))
+  
+@program_bp.route('/edit_program', methods = ['POST'])
+def edit_program():
+  if request.method == 'POST':
+    progInitial = request.form['progInitial']
+    progCode = request.form['codeEdit']
+    progName = request.form['nameEdit']
+    progColCode = request.form['colEdit']
+
+    program = [progCode, progName, progColCode, progInitial]
+    editProgram(program)
+    flash('Program Edited Succesfully')
+    
+    return redirect(url_for('general.programs'))
