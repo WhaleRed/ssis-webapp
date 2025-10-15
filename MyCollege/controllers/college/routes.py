a1c7482451a0311bb61bedee8dd84120eec30e47
WhaleRed <redwhalecs@gmail.com>
2025-10-15 16:11:41 +0800

    feat(server-side): Server-side pagination and search

--- MyCollege/controllers/college/routes.py
+++ MyCollege/controllers/college/routes.py
@@ -1,13 +1,30 @@
 from flask import request, jsonify
 from . import college_bp
-from MyCollege.models.college import addCollege, editCollege, deleteCollege, getAllColleges
+from MyCollege.models.college import *
 
-@college_bp.route('/college/data')
+@college_bp.route('/college/data', methods=['POST'])
 def get_colleges_data():
     try:
-        colleges = getAllColleges() 
-        data = [{'code': c[0], 'name': c[1]} for c in colleges]
-        return jsonify({'data': data})
+        #DataTables Parameter for server side
+        draw = int(request.form.get('draw', 1))
+        start = int(request.form.get('start', 0))
+        length = int(request.form.get('length', 10))
+        search_value = request.form.get('search[value]', '')
+
+        #Data retrieval
+        retrieve = getAllColleges(search=search_value, start=start, length=length)
+        total_records = getCollegeCount()
+        filtered_records = getCollegeCount(search=search_value)
+
+        data = [{'code': c[0], 'name': c[1]} for c in retrieve]
+
+        return jsonify ({
+            'draw': draw,
+            'recordsTotal': total_records,
+            'recordsFiltered': filtered_records,
+            'data': data
+        })
+
     except Exception as e:
         return jsonify({'data': [], 'error': str(e)})
 
@@ -47,10 +64,3 @@ def delete_college():
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})
     
-@college_bp.route('/data')
-def get_college_data():
-    colleges = getAllColleges()
-
-    data = [{'code': c[0], 'name': c[1]} for c in colleges]
-
-    return jsonify({'data': data})
